e713443c44d618d14c6cd54e5c3e5640e9b65583
adityabe4better <aditya@be4better.com>
2020-11-24 20:11:01 +0530

    Added herobar and nav

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+function Home() {
+    return (
+        <div className='home'>
+            <div className="home__container">
+                <img
+                    className="home__image"
+                    src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2020/Holiday/GiftGuide/Fuji_TallHero_GG2_en_US_1x._CB418256337_.jpg" alt="" srcset="" />
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBzn7a0k_1sYJeRoQqrsKQDlav7-2G0dxw",
+    authDomain: "clone-330a0.firebaseapp.com",
+    databaseURL: "https://clone-330a0.firebaseio.com",
+    projectId: "clone-330a0",
+    storageBucket: "clone-330a0.appspot.com",
+    messagingSenderId: "105945510799",
+    appId: "1:105945510799:web:f8f9c6dfe503ddeaf711fa",
+    measurementId: "G-KJV5L3HXY7"
+  };
